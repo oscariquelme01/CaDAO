dadf8474f6bc887302691b7d4a9258b18bdb4729
oscariquelme01 <oscariquelme01@gmail.com>
2022-09-28 11:40:39 +0200

    add accounts to hardhad local dev network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,18 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import "@nomicfoundation/hardhat-toolbox"
 
+import * as secrets from "./secrets.json"
+
 const config: HardhatUserConfig = {
     defaultNetwork: "localhost",
     solidity: "0.8.17",
     networks: {
         hardhat: {
-            chainId: 31337
+            chainId: 31337,
         },
         localhost: {
-            chainId: 31337
+            chainId: 31337,
+            accounts: secrets.accounts.hardhat_keys
         }
     }
 }
