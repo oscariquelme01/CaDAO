6c414f6ed9c07b422820cb4c71480ca7c89354aa
oscariquelme01 <oscariquelme01@gmail.com>
2022-09-28 16:18:43 +0200

    mineblocks is now a script running in the background that simulates the blockchain time

--- scripts/mineBlock.ts
+++ scripts/mineBlock.ts
@@ -1,9 +1,24 @@
 import { ethers } from "hardhat"
 
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function mineNBlocks(n: number) {
     for (let index = 0; index < n; index++) {
         await ethers.provider.send('evm_mine');
     }
 }
 
-mineNBlocks(10)
+async function main() {
+    while (true) {
+        // Wait 13 seconds and mine 1 block 
+        mineNBlocks(1)
+        await delay(13000)
+    }
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
